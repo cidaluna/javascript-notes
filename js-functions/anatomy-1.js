f1271b20bf83774792fbd9313589c84462e51143
Cida Luna <cidalunna@hotmail.com>
2024-03-15 12:49:52 -0300

    function declaration

---
+++ js-functions/anatomy-1.js
@@ -0,0 +1,39 @@
+// CTRL + Alt + n para rodar o code runner
+
+// Função 1
+function sayHello(){
+    console.log('Hello!')
+}
+
+// Invocando a função
+sayHello()
+
+// Função 2 com parâmetro
+function sayHelloTo(name){
+    console.log('Hello '+ name)
+}
+
+// Invocando a função com parâmetros
+sayHelloTo('Cida')
+
+// Função 3 com parâmetro
+function sayHelloTo2(name){
+    console.log(`Hello: ${name}`)
+}
+
+sayHelloTo2('Luna')
+
+// Função 4 com retorno
+function returnHi(){
+    return 'Hi!'
+}
+
+let greeting = returnHi()
+console.log(greeting)
+console.log(returnHi())
+
+function returnHiTo(name){
+    return `Hi ${name}!!!`
+}
+
+console.log(returnHiTo('John'))
