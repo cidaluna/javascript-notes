2316ec3fc311c092300269e82acdd91fdd3f388b
Cida Luna <cidalunna@hotmail.com>
2024-03-15 20:30:49 -0300

    output with document write

---
+++ js-functions/function-challenge1.js
@@ -0,0 +1,14 @@
+// Create a range function
+// range(5) -> [1,2,3,4,5]
+
+function rangeNums(...nums){
+    for(let i=1; i<= nums; i++){
+        console.log(i)
+    }
+    if(nums == 6,11){
+        
+    }
+}
+
+rangeNums(5)
+rangeNums(6,11)
