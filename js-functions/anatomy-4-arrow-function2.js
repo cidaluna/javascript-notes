8eb7d8683b39be0f78d69f45915dd548c82aeba8
Cida Luna <cidalunna@hotmail.com>
2024-03-15 13:17:58 -0300

    another arrow function

---
+++ js-functions/anatomy-4-arrow-function2.js
@@ -0,0 +1,15 @@
+// Anonymous function
+(function (a,b,c){
+    console.log(`Result: ${a+b+c}`)
+})(1,2,3);
+
+(function(){
+    let x = 300
+    console.log(x)
+})();
+
+(()=> {
+    console.log('arrow 1')
+})();
+
+(()=> console.log('arrow 2'))();
